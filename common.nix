f1f1fc544e1a896b0a26115b32d7fb1f36fa880c
Jarrod Pas <jarrod@jarrodpas.com>
2020-01-31 01:04:37 -0600

    feat: keybase daemon

--- common.nix
+++ common.nix
@@ -22,6 +22,10 @@
     tmux
   ];
 
+  # Enable the Keybase daemon and filesystem.
+  services.keybase.enable = true;
+  services.kbfs.enable = true;
+
   # The global useDHCP flag is deprecated, therefore explicitly set to false here.
   # Per-interface useDHCP will be mandatory in the future, so this generated config
   # replicates the default behaviour.
