efbd3d32cc96f079f91a064775c431bdcbb6c0ba
Jarrod Pas <jarrod@jarrodpas.com>
2020-01-31 01:05:06 -0600

    feat: gtk theme

--- role/desktop.nix
+++ role/desktop.nix
@@ -22,4 +22,57 @@
       extraPackages = with pkgs; [ i3lock dmenu i3status ];
     };
   };
+
+  #environment.extraInit = ''
+  #  rm -f ~/.config/Trolltech.conf
+  #  rm -f ~/.config/gtk-3.0/settings.ini
+
+  #  # GTK3: add arc theme to search path for themes
+  #  export XDG_DATA_DIRS="${pkgs.arc-theme}/share:$XDG_DATA_DIRS"
+
+
+  #  # ensure xdg home is set up properly
+  #  export XDG_CONFIG_HOME=$HOME/.config
+  #  export XDG_DATA_HOME=$HOME/.local/share
+  #  export XDG_CACHE_HOME=$HOME/.cache
+  #'';
+
+  # QT4/5 global theme
+  #environment.etc."xdg/Trolltech.conf" = {
+  #  text = ''
+  #    [Qt]
+  #    style=${theme.qt}
+  #  '';
+  #  mode = "444";
+  #};
+
+  qt5 = {
+    enable = true;
+    style = "gtk2";
+    platformTheme = "gtk2";
+  };
+
+  environment.etc."gtk-2.0/gtkrc" = {
+    text = ''
+      gtk-icon-theme-name="Papirus-Dark"
+      gtk-theme-name="Arc-Dark"
+    '';
+    mode = "444";
+  };
+
+  environment.etc."gtk-3.0/settings.ini" = {
+    text = ''
+      [Settings]
+      gtk-icon-theme-name=Papirus-Dark
+      gtk-theme-name=Arc-Dark
+    '';
+    mode = "444";
+  };
+
+  environment.systemPackages = with pkgs; [
+    papirus-icon-theme
+    arc-theme
+  ];
+
+  #environment.pathsToLink = [ "/share" ];
 }
