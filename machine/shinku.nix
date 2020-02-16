d7733657940b18ab0439ea7a96b3cfbb3206c75a
Jarrod Pas <jarrod@jarrodpas.com>
2020-02-16 00:38:07 -0600

    rice: remove kde

--- machine/shinku.nix
+++ machine/shinku.nix
@@ -32,10 +32,5 @@
   # Configure network proxy if necessary
   # networking.proxy.default = "http://user:password@proxy:port/";
   # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
-    
-  # Enable the KDE Desktop Environment.
-  services.xserver = {
-    desktopManager.plasma5.enable = true;
-  };
 }
 
