e99a39b49b52f8a45008c69a44473ba27d1b41d3
Jarrod Pas <jarrod@jarrodpas.com>
2020-10-27 15:42:21 -0600

    Clean up bat config

--- modules/home/bat.nix
+++ modules/home/bat.nix
@@ -1,16 +1,21 @@
-{ pkgs, ... }: {
+{ pkgs
+, ...
+}:
+let
+  gruvbox-theme = pkgs.fetchFromGitHub {
+    owner = "austinwagner";
+    repo = "gruvbox-sublime";
+    rev = "0fe9cb7208c17ef318b573ba9ab44e0becc59a71";
+    sha256 = "1l9gnpry0ijmlkdh6qr5njyn1yd96ahlhhgvh8kb7cmmz5kymyd0";
+  };
+in {
   programs.bat = {
     enable = true;
 
     config = { theme = "gruvbox"; };
 
     themes = {
-      gruvbox = builtins.readFile (pkgs.fetchFromGitHub {
-        owner = "austinwagner";
-        repo = "gruvbox-sublime";
-        rev = "0fe9cb7208c17ef318b573ba9ab44e0becc59a71";
-        sha256 = "1l9gnpry0ijmlkdh6qr5njyn1yd96ahlhhgvh8kb7cmmz5kymyd0";
-      } + "/gruvbox.tmTheme");
+      gruvbox = builtins.readFile "${gruvbox-theme}/gruvbox.tmTheme";
     };
   };
 }
