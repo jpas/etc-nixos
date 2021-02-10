9ce09833c4f824dcee745cc1d56319525bdc180e
Jarrod Pas <jarrod@jarrodpas.com>
2021-02-10 17:52:07 -0600

    use iwd instead of wpa_supplicant

--- profiles/base.nix
+++ profiles/base.nix
@@ -66,6 +66,8 @@
     layout = "us";
   };
 
+  networking.networkmanager.wifi.backend = "iwd";
+
   # The global useDHCP flag is deprecated, therefore explicitly set to false here.
   # Per-interface useDHCP will be mandatory in the future, so this generated config
   # replicates the default behaviour.
@@ -78,6 +80,7 @@
   };
 
   services.resolved.enable = true;
+
   services.tailscale.enable = true;
 
   users = {
