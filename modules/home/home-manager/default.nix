2a693e3c50b24d3ec659697598994d7218e6226e
Jarrod Pas <jarrod@jarrodpas.com>
2021-05-13 17:50:27 -0600

    wip

---
+++ modules/home/home-manager/default.nix
@@ -0,0 +1,8 @@
+{
+  hole = ../hole/home;
+
+  bat = ../home-manager/bat.nix;
+  imv = ../home-manager/imv.nix;
+  oauth2ms = ../home-manager/oauth2ms.nix;
+  signal = ../home-manager/signal.nix;
+}
