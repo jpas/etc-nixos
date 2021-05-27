28b6ac314159def30b8d70df4ee376e9c21dd578
Jarrod Pas <jarrod@jarrodpas.com>
2021-05-27 17:06:42 -0600

    wip

--- lib/compat/channels/nixos.nix
+++ lib/compat/channels/nixos.nix
@@ -2,4 +2,4 @@ let
   flake = builtins.getFlake "hole";
   hostname = flake.lib.fileContents /etc/hostname;
 in
-  flake.nixosConfigurations."${hostname}".config.nixpkgs.pkgs;
+  flake.nixosConfigurations."${hostname}".config.nixpkgs.pkgs
