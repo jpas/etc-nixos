6c8b0cdd6a21956ca3cc3fc01f7c1109192cec79
Jarrod Pas <jarrod@jarrodpas.com>
2021-05-31 14:36:08 -0600

    wip

--- users/kbell.nix
+++ users/kbell.nix
@@ -4,6 +4,9 @@
     uid = 1001;
     isNormalUser = true;
     passwordFile = "/etc/nixos/secrets/passwd.d/kbell";
+    openssh.authorizedKeys.keys = [
+      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIMXyVMHbf69zSybXTmyQc/CHjx7j56O/VAl7N/KsMREw jpas@kuro"
+    ];
   };
   home-manager.users.kbell = import ./kbell;
 }
