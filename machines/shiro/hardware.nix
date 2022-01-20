398888eff20fd43dabbb1325dec0f81afff3bd0c
Jarrod Pas <jarrod@jarrodpas.com>
2022-01-20 17:20:54 -0500

    fix swap devices

--- machines/shiro/hardware.nix
+++ machines/shiro/hardware.nix
@@ -48,6 +48,8 @@
     };
 
   swapDevices = [
-    device = "/dev/disk/by-uuid/f1476467-c295-47c7-8d0b-51dbcaf182dd";
+    {
+      device = "/dev/disk/by-uuid/f1476467-c295-47c7-8d0b-51dbcaf182dd";
+    }
   ];
 }
