ea309cb683f01e2fb069fe4cf89adfc17027d81b
Jarrod Pas <jarrod@jarrodpas.com>
2022-03-22 14:05:51 -0400

    machines.doko: sr.ht

--- machines/doko/hardware.nix
+++ machines/doko/hardware.nix
@@ -1,4 +1,13 @@
-{ ... }:
+{ lib
+, ...
+}:
+
+with lib;
+
+# Notes:
+# fan control via ipmi
+# https://github.com/missmah/ipmi_tools/blob/master/ipmi_fancontrol.pl
+
 {
   imports = [
     ../hardware/intel-cpu.nix
@@ -12,24 +21,46 @@
     device = "/dev/disk/by-uuid/75b16615-d495-4fb9-9150-4febc1c6f25e";
   };
 
-  fileSystems."/" =
-    { device = "/dev/disk/by-uuid/3cc56ffc-1714-4463-9cf4-902a883eafb5";
+  fileSystems = {
+    "/" = {
+      device = "/dev/disk/by-uuid/3cc56ffc-1714-4463-9cf4-902a883eafb5";
       fsType = "btrfs";
       options = [ "relatime" "subvol=@" "compress=zstd" "space_cache=v2" ];
     };
 
-  fileSystems."/nix" =
-    { device = "/dev/disk/by-uuid/3cc56ffc-1714-4463-9cf4-902a883eafb5";
+    "/nix" = {
+      device = "/dev/disk/by-uuid/3cc56ffc-1714-4463-9cf4-902a883eafb5";
       fsType = "btrfs";
       options = [ "noatime" "subvol=@nix" "compress=zstd" "space_cache=v2" ];
     };
 
-  fileSystems."/boot" =
-    { device = "/dev/disk/by-uuid/1874-8176";
+    "/boot" = {
+      device = "/dev/disk/by-uuid/1874-8176";
       fsType = "vfat";
     };
 
-  swapDevices =
-    [ { device = "/dev/disk/by-uuid/b022d055-04e1-4ce6-ab01-6b66d47fd35a"; }
-    ];
+  };
+
+  swapDevices = [
+    { device = "/dev/disk/by-uuid/b022d055-04e1-4ce6-ab01-6b66d47fd35a"; }
+  ];
+
+  systemd.network.networks = {
+    "00-unmanaged" = {
+      linkConfig = {
+        Unmanaged = true;
+      };
+
+      matchConfig.Name = [
+        # "enp7s0"
+        "enp8s0"
+        "enp11s0f0"
+        "enp11s0f1"
+        "enp11s0f2"
+        "enp11s0f3"
+        "enp4s0f0"
+        "enp4s0f1"
+      ];
+    };
+  };
 }
--- machines/doko/srht.nix
+++ machines/doko/srht.nix
@@ -1,5 +1,6 @@
 { lib
 , config
+, pkgs
 , ...
 }:
 
@@ -7,74 +8,104 @@ with lib;
 
 let
   cfg = config.services.sourcehut;
-  fqdn = config.settings."sr.ht".global-domain;
+  fqdn = "ht.pas.sh";
+  secrets-root = "/etc/nixos/secrets/srht";
 in
-{
-  services.sourcehut.enable = true;
-  services.sourcehut.settings."sr.ht".global-domain = "ht.pas.sh";
+mkMerge [
+  {
+    services.sourcehut.enable = true;
+    services.sourcehut = {
+      meta.enable = true;
+      git.enable = true;
 
-  services.sourcehut = {
-    meta.enable = true;
-    git.enable = true;
+      redis.enable = true;
 
-    settings = {
-      "sr.ht" = {
-        environment = "production";
-        origin = "https://${fqdn}";
-        network-key = "/etc/nixos/secrets/srht/network-key";
-        service-key = "/etc/nixos/secrets/srht/service-key";
+      settings = {
+        "sr.ht" = {
+          environment = "production";
+          global-domain = fqdn;
+          origin = "https://${fqdn}";
+          network-key = "${secrets-root}/network-key";
+          service-key = "${secrets-root}/service-key";
+        };
+
+        webhooks = {
+          private-key = "${secrets-root}/webhooks-private-key";
+        };
+
+        mail = {
+          smtp-host = "smtp.fastmail.com";
+          smtp-port = 587;
+          smtp-encryption = "starttls";
+
+          smtp-user = "<${secrets-root}/smtp-user";
+          smtp-password = "<${secrets-root}/smtp-password";
+          smtp-from = "<${secrets-root}/smtp-from";
+        };
+      };
+    };
+  }
+
+  {
+    services.sourcehut = {
+      meta = {
+        enable = true;
+        redis.host = "unix:///run/redis-sourcehut-metasrht/redis.sock?db=0";
       };
 
-      webhooks = {
-        private-key = "/etc/nixos/secrets/srht/webhooks-private-key";
+      settings."meta.sr.ht" = {
+        webhooks = "unix:///run/redis-sourcehut-metasrht/redis.sock?db=1";
+      };
+
+      # TODO: settings."meta.sr.ht::auth".auth-method = "unix-pam";
+    };
+
+    systemd.services.metasrht-webhooks.enable = false;
+  }
+
+  {
+    services.sourcehut = {
+      git = {
+        enable = true;
+        redis.host = "unix:///run/redis-sourcehut-gitsrht/redis.sock?db=0";
+      };
+
+      settings."git.sr.ht" = {
+        oauth-client-id = "</etc/nixos/secrets/srht/git-oauth-client-id";
+        oauth-client-secret = "/etc/nixos/secrets/srht/git-oauth-client-secret";
+        webhooks = "unix:///run/redis-sourcehut-gitsrht/redis.sock?db=1";
       };
     };
 
-  services.sourcehut.meta.enable = true;
-  services.sourcehut = {
-    meta = {
-      redis.host = "unix:///run/redis-sourcehut-metasrht/redis.sock?db=0";
+    systemd.services.gitsrht-webhooks.enable = false;
+  }
+
+  {
+    services.sourcehut.postgresql.enable = true;
+    services.postgresql = {
+      enable = mkIf cfg.postgresql.enable true;
+      package = pkgs.postgresql_11;
+    };
+  }
+
+  {
+    services.sourcehut.nginx.enable = true;
+    services.sourcehut.nginx = {
+      virtualHost.useACMEHost = fqdn;
     };
 
-    settings."meta.sr.ht" = {
-      webhooks = "unix:///run/redis-sourcehut-metasrht/redis.sock?db=1";
-    };
-  };
-
-  services.sourcehut.git.enable = true;
-  services.sourcehut = {
-    git = {
-      redis.host = "unix:///run/redis-sourcehut-gitsrht/redis.sock?db=0";
+    security.acme.certs."${fqdn}" = {
+      group = "nginx";
+      extraDomainNames = [ "*.${fqdn}" ];
     };
 
-    settings."git.sr.ht" = {
-      oauth-client-id = "9c76febb007a0eb4";
-      oauth-client-secret = "/etc/nixos/secrets/srht/git-oauth-secret";
-      webhooks = "unix:///run/redis-sourcehut-gitsrht/redis.sock?db=1";
+    services.nginx = {
+      enable = mkIf cfg.nginx.enable true;
+
+      recommendedTlsSettings = true;
+      recommendedOptimisation = true;
+      recommendedGzipSettings = true;
+      recommendedProxySettings = true;
     };
-  };
-
-  services.sourcehut.redis.enable = true;
-
-  services.sourcehut.postgresql.enable = true;
-  services.postgresql = {
-    enable = mkIf cfg.postgresql.enable true;
-    package = pkgs.postgresql_11;
-  };
-
-  security.acme.certs."${fqdn}".extraDomainNames = [ "*.${fqdn}" ];
-
-  services.sourcehut.nginx.enable = true;
-  services.sourcehut.nginx = {
-    virtualHost.useACMEHost = fqdn;
-  };
-
-  services.nginx = {
-    enable = mkIf cfg.nginx.enable true;
-
-    recommendedTlsSettings = true;
-    recommendedOptimisation = true;
-    recommendedGzipSettings = true;
-    recommendedProxySettings = true;
-  };
-}
+  }
+]
