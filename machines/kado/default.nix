e6e53e55873f8a50b40dd89da64a69360e772244
Jarrod Pas <jarrod@jarrodpas.com>
2022-06-05 18:18:39 -0400

    machines.kado: fix networking

--- machines/kado/default.nix
+++ machines/kado/default.nix
@@ -32,4 +32,20 @@
   services.fail2ban.enable = true;
 
   virtualisation.docker.enable = true;
+
+  systemd.network.networks = {
+    "01-unmanaged" = {
+      linkConfig = {
+        Unmanaged = true;
+        RequiredForOnline = "no";
+      };
+
+      matchConfig.Name = [
+        # "enp0s20f0"
+        "enp0s20f1"
+        "enp0s20f2"
+        "enp0s20f3"
+      ];
+    };
+  };
 }
