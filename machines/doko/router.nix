63061d2b8ed941c890e086df0e0853823d8f47cb
Jarrod Pas <jarrod@jarrodpas.com>
2022-11-21 01:13:57 -0500

    wip

--- machines/doko/router.nix
+++ machines/doko/router.nix
@@ -24,6 +24,12 @@ mkIf true {
       };
       networkConfig = {
         DHCP = "yes";
+        DNS = [
+          "1.1.1.1#cloudflare-dns.com"
+          "1.0.0.1#cloudflare-dns.com"
+          "2606:4700:4700::1111#cloudflare-dns.com"
+          "2606:4700:4700::1001#cloudflare-dns.com"
+        ];
         IPv6AcceptRA = true;
         IPMasquerade = true;
       };
