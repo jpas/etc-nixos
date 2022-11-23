6ee60563fa2753c8a01316118428f5e0ea4f6e5b
Jarrod Pas <jarrod@jarrodpas.com>
2022-11-22 21:53:18 -0500

    wip

--- profiles/base/networking/default.nix
+++ profiles/base/networking/default.nix
@@ -12,18 +12,21 @@ with lib;
   networking.useNetworkd = mkDefault true;
   systemd.services.systemd-networkd-wait-online.enable = mkDefault false;
 
-  systemd.network = {
-    links."98-default" = {
-      matchConfig.OriginalName = "*";
-      linkConfig = {
-        NamePolicy = "keep kernel database onboard slot path mac";
-        AlternativeNamesPolicy = "database onboard slot path mac";
-        MACAddressPolicy = "persistent";
-      };
+  systemd.network.links."98-default" = {
+    matchConfig.Name = "*";
+    linkConfig = {
+      NamePolicy = "keep kernel database onboard slot path mac";
+      AlternativeNamesPolicy = "database onboard slot path mac";
+      MACAddressPolicy = "persistent";
     };
-    networks."98-default" = {
-      matchConfig.OriginalName = "*";
-      linkConfig.Unmanaged = true;
-    };
+  };
+
+  systemd.network.networks."20-lan" = {
+    dhcpV4Config.ClientIdentifier = "mac";
+  };
+
+  systemd.network.networks."98-default" = {
+    matchConfig.Name = "*";
+    linkConfig.Unmanaged = true;
   };
 }
