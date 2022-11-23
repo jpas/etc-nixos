befec89942b214f8bee67528c6fe8dee1de7bd9a
Jarrod Pas <jarrod@jarrodpas.com>
2022-11-22 21:54:40 -0500

    wip

--- profiles/base/networking/default.nix
+++ profiles/base/networking/default.nix
@@ -22,7 +22,7 @@ with lib;
   };
 
   systemd.network.networks."20-lan" = {
-    dhcpV4Config.ClientIdentifier = mkDeafult "mac";
+    dhcpV4Config.ClientIdentifier = mkDefault "mac";
   };
 
   systemd.network.networks."98-default" = {
