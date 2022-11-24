9bdfc684dd54a5592718cf08370790a53fa3a16b
Jarrod Pas <jarrod@jarrodpas.com>
2022-11-24 18:26:13 -0500

    wip

--- machines/kado/nfs.nix
+++ machines/kado/nfs.nix
@@ -1,8 +1,8 @@
 {
   services.nfs.server.enable = true;
   services.nfs.server.exports = ''
-    /srv/exports        10.39.1.1(insecure,rw,async,no_subtree_check,crossmnt,fsid=root)
-    /srv/exports/aleph  10.39.1.1(insecure,rw,async,no_subtree_check)
+    /srv/exports        10.39.1.254(insecure,rw,async,no_subtree_check,crossmnt,fsid=root)
+    /srv/exports/aleph  10.39.1.254(insecure,rw,async,no_subtree_check)
   '';
 
   fileSystems."/srv/exports/aleph" = {
