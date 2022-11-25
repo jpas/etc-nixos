045374cbd99b4575b983eb04f12554eb81c068a7
Jarrod Pas <jarrod@jarrodpas.com>
2022-11-24 19:43:01 -0500

    wip

--- machines/doko/jellyfin.nix
+++ machines/doko/jellyfin.nix
@@ -9,8 +9,8 @@
       rule = concatMapStringsSep "||" (r: "(${r})") [
         "Host(`jellyin.pas.sh`)"
         "Host(`jellyin.o.pas.sh`) && ClientIP(`100.64.0.0/10`, fd7a:115c:a1e0:ab12::/64`)"
-      ]
-        service = "jellyfin@file";
+      ];
+      service = "jellyfin@file";
       entryPoints = [ "web" ];
     };
   };
