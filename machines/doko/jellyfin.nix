7dfe1be16bb803302af80274158839b135de113b
Jarrod Pas <jarrod@jarrodpas.com>
2022-11-24 19:44:53 -0500

    wip

--- machines/doko/jellyfin.nix
+++ machines/doko/jellyfin.nix
@@ -8,7 +8,7 @@ with lib;
       { url = "http://10.39.1.20:8096"; }
     ];
     http.routers.jellyfin = {
-      rule = concatMapStringsSep "||" (r: "(${r})") [
+      rule = concatMapStringsSep " || " (r: "(${r})") [
         "Host(`jellyin.pas.sh`)"
         "Host(`jellyin.o.pas.sh`) && ClientIP(`100.64.0.0/10`, `fd7a:115c:a1e0:ab12::/64`)"
       ];
