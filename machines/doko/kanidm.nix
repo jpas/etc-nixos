4ffe3878513eb520700a61d03bf34ab804d559d3
Jarrod Pas <jarrod@jarrodpas.com>
2022-11-26 12:43:56 -0500

    wip

--- machines/doko/kanidm.nix
+++ machines/doko/kanidm.nix
@@ -14,6 +14,8 @@ with lib;
     tls_key = "${config.security.acme.certs."idm.pas.sh".directory}/key.pem";
   };
 
+  systemd.tmpfiles.rules = [ "d /var/lib/kanidm 0700 kanidm kanidm - -" ];
+
   services.traefik.dynamicConfigOptions.http = {
     services.kanidm = {
       loadBalancer.servers = [{ url = "http://127.0.0.1:8443"; }];
