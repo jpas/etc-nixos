dd96e73d77a388de4b633efeb12c106a5f8f908e
Jarrod Pas <jarrod@jarrodpas.com>
2022-11-25 23:07:03 -0500

    wip

--- machines/doko/traefik/radarr.nix
+++ machines/doko/traefik/radarr.nix
@@ -5,7 +5,7 @@ with lib;
 {
   services.traefik.dynamicConfigOptions = {
     http.services.radarr.loadBalancer.servers = [
-      { url = "http://10.39.1.20:8989"; }
+      { url = "http://10.39.1.20:7878"; }
     ];
     http.routers.radarr = {
       rule = "Host(`radarr.o.pas.sh`) && ClientIP(`100.64.0.0/10`, `fd7a:115c:a1e0:ab12::/64`)";
