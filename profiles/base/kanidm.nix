abf936e71d4590d40773f1bbe11e53155e25c9a1
Jarrod Pas <jarrod@jarrodpas.com>
2022-11-26 14:44:44 -0500

    wip

--- profiles/base/kanidm.nix
+++ profiles/base/kanidm.nix
@@ -8,4 +8,10 @@ with lib;
   services.kanidm.clientSettings = {
     uri = "https://idm.pas.sh";
   };
+
+  services.kanidm.enablePam = true;
+  services.kanidm.unixSettings = {
+    pam_allowed_login_groups = [ "users" ];
+    default_shell = "/run/current-system/sw/bin/bash";
+  };
 }
