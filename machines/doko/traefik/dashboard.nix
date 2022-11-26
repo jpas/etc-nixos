4bb7bffe63dfffb6a603eacbcee67dd438af28ca
Jarrod Pas <jarrod@jarrodpas.com>
2022-11-25 23:13:48 -0500

    wip

--- machines/doko/traefik/dashboard.nix
+++ machines/doko/traefik/dashboard.nix
@@ -6,26 +6,16 @@ let
   cfg = config.services.traefik;
 in
 {
-  options.services.traefik = {
-    dashboard = mkEnableOption "traefik dashboard";
+  services.traefik.dynamicConfigOptions = {
+    http.routers.dashboard = {
+      rule = "Host(`traefik.o.pas.sh`) && ClientIP(`100.64.0.0/10`, `fd7a:115c:a1e0:ab12::/64`)";
+      service = "api@internal";
+      entryPoints = [ "web" ];
+      middlewares = [ "auth" ];
+    };
   };
 
-  config = mkIf cfg.dashboard {
-    services.traefik.staticConfigOptions = {
-      api.dashboard = mkForce true;
-    };
-
-    services.traefik.dynamicConfigOptions = {
-      http.routers.dashboard = {
-        rule = "Host(`traefik.o.pas.sh`) && ClientIP(`100.64.0.0/10`, `fd7a:115c:a1e0:ab12::/64`)";
-        service = "api@internal";
-        entryPoints = [ "web" ];
-        middlewares = [ "auth" ];
-      };
-    };
-
-    services.authelia.settings.access_control.rules = [
-      { domain = "traefik.o.pas.sh"; policy = "one_factor"; subject = [ "group:wheel" ]; }
-    ];
-  };
+  services.authelia.settings.access_control.rules = [
+    { domain = "traefik.o.pas.sh"; policy = "one_factor"; subject = [ "group:wheel" ]; }
+  ];
 }
