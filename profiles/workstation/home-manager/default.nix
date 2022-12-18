622960ea6a5e557f57b5f961da69a12ded68047d
Jarrod Pas <jarrod@jarrodpas.com>
2022-12-18 17:09:57 -0500

    profiles.workstation: make zathura global

--- profiles/workstation/home-manager/default.nix
+++ profiles/workstation/home-manager/default.nix
@@ -11,10 +11,7 @@ with lib;
     ./neovim.nix
     ./sway.nix
     ./tmux.nix
-    ./zathura.nix
   ];
 
   home.packages = [ pkgs.signal-desktop ];
-
-  programs.zathura.enable = mkDefault true;
 }
--- profiles/workstation/zathura.nix
+++ profiles/workstation/zathura.nix
@@ -1,8 +1,18 @@
-{ lib, ... }:
+{ lib, config, pkgs, ... }:
 
 with lib;
 
+let
+  colours = config.hole.colours.fmt (c: "#${c}");
+
+  format = settings: pipe settings [
+    (mapAttrsToList (name: value: "set ${name} \"${toString value}\""))
+    (concatStringsSep "\n")
+  ];
+in
 {
+  environment.systemPackages = [ pkgs.zathura ];
+
   xdg.mime.addedAssociations = {
     "application/pdf" = [ "org.pwmt.zathura.desktop" ];
   };
@@ -14,4 +24,56 @@ with lib;
   environment.etc."sway/config.d/zathura.conf".text = ''
     for_window [app_id="org.pwmt.zathura"] floating enable
   '';
+
+  environment.etc."zathurarc".text = format (with colours; {
+    font = "monospace 10";
+
+    selection-clipboard = "clipboard";
+    guioptions = "";
+    window-title-home-tilde = true;
+    window-title-page = true;
+
+    default-bg = bg;
+    default-fg = fg;
+
+    notification-error-bg = bg;
+    notification-error-fg = neutral.red;
+    notification-warning-bg = bg;
+    notification-warning-fg = neutral.yellow;
+    notification-bg = bg;
+    notification-fg = fg;
+
+    completion-bg = bg2;
+    completion-fg = fg;
+    completion-group-bg = bg1;
+    completion-group-fg = fg;
+    completion-highlight-bg = bright.aqua;
+    completion-highlight-fg = bg;
+
+    index-bg = bg2;
+    index-fg = fg;
+    index-active-bg = neutral.aqua;
+    index-active-fg = bg;
+
+    inputbar-bg = bg;
+    inputbar-fg = fg;
+
+    statusbar-bg = bg1;
+    statusbar-fg = fg;
+
+    statusbar-h-padding = 10;
+    statusbar-v-padding = 4;
+
+    highlight-color = neutral.yellow;
+    highlight-active-color = neutral.orange;
+
+    render-loading = true;
+    render-loading-bg = bg;
+    render-loading-fg = fg;
+
+    recolor = true;
+    # swapped since lightcolor is usually white and dark is prefered.
+    recolor-lightcolor = bg;
+    recolor-darkcolor = fg;
+  });
 }
