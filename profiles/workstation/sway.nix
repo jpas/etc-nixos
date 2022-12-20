e65289c4d6303629d78bb7769042b7d5a27c5013
Jarrod Pas <jarrod@jarrodpas.com>
2022-12-20 13:52:55 -0500

    profiles.workstation: improve sway session handling

--- profiles/workstation/sway.nix
+++ profiles/workstation/sway.nix
@@ -5,9 +5,43 @@ with lib;
 let
   cfg = config.programs.sway;
 
-  sway-launch = pkgs.writeShellScriptBin "sway-launch" ''
-    exec systemd-cat --identifier=sway sway "$@"
-  '';
+  sessionVariables = concatStringsSep " " [
+    "DISPLAY"
+    "I3SOCK"
+    "SWAYSOCK"
+    "WAYLAND_DISPLAY"
+    "XDG_CURRENT_DESKTOP"
+    "XDG_SESSION_DESKTOP"
+    "XDG_SESSION_TYPE"
+  ];
+
+  sway-utils = pkgs.symlinkJoin {
+    name = "sway-utils";
+    paths = mapAttrsToList pkgs.writeShellScriptBin {
+      sway-launch = ''
+        if systemctl --user --quiet is-active sway-session.target; then
+          echo "sway is already running, refusing to start." 1>&2
+          exit 1
+        fi
+        exec systemd-cat --identifier=sway sway "$@"
+      '';
+
+      sway-session = ''
+        if ! systemctl --user --quiet is-active sway-session.target; then
+          dbus-update-activation-environment ${sessionVariables}
+          systemctl --user import-environment ${sessionVariables}
+          systemctl --user reset-failed
+        fi
+        systemctl --user start sway-session.target
+      '';
+
+      sway-exit = ''
+        systemctl --user stop sway-session.target
+        systemctl --user unset-environment ${sessionVariables}
+        swaymsg exit
+      '';
+    };
+  };
 in
 mkIf cfg.enable {
   services.greetd = {
@@ -27,15 +61,17 @@ mkIf cfg.enable {
     };
 
     extraSessionCommands = ''
-      export SDL_VIDEODRIVER=wayland
+      export XDG_SESSION_DESKTOP=sway
 
       # Fix for some Java AWT applications (e.g. Android Studio),
       # use this if they aren't displayed properly:
       export _JAVA_AWT_WM_NONREPARENTING=1
+
+      export SDL_VIDEODRIVER=wayland
     '';
 
     extraPackages = attrValues {
-      inherit sway-launch;
+      inherit sway-utils;
       inherit (pkgs)
         grim
         kanshi
@@ -48,11 +84,8 @@ mkIf cfg.enable {
   };
 
   environment.etc."sway/config.d/nixos.conf".enable = mkForce false;
-  environment.etc."sway/config.d/10-systemd.conf".text = ''
-    exec_always "${concatStringsSep "; " [
-      "dbus-update-activation-environment --systemd DISPLAY WAYLAND_DISPLAY SWAYSOCK XDG_CURRENT_DESKTOP"
-      "systemctl --user start sway-session.target"
-    ]}"
+  environment.etc."sway/config.d/10-session.conf".text = ''
+    exec_always ${sway-utils}/bin/sway-session
   '';
 
   systemd.user.targets."sway-session" = {
