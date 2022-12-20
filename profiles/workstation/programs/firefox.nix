a46a1835278fb0ed4934932543a553e3b0e2bd25
Jarrod Pas <jarrod@jarrodpas.com>
2022-12-20 05:25:29 -0500

    profiles.workstation: firefox autoconfig

--- profiles/workstation/programs/firefox.nix
+++ profiles/workstation/programs/firefox.nix
@@ -3,7 +3,13 @@
 with lib;
 
 {
-  environment.systemPackages = [ pkgs.firefox ];
+  programs.firefox.enable = true;
+  programs.firefox.autoConfig = ''
+    lockPref("extensions.pocket.enabled", false)
+    lockPref("signon.rememberSignons", false)
+    lockPref("pref.general.disable_button.default_browser", true)
+    lockPref("pref.privacy.disable_button.view_passwords", true)
+  '';
 
   programs.sway.extraSessionCommands = ''
     export MOZ_ENABLE_WAYLAND=1
