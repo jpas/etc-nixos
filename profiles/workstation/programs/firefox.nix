729aa87693de7ea42c4b11db5f1b68e41125daa5
Jarrod Pas <jarrod@jarrodpas.com>
2023-01-20 13:13:03 -0500

    programs.firefox: change defaults through policy

--- profiles/workstation/programs/firefox.nix
+++ profiles/workstation/programs/firefox.nix
@@ -1,29 +1,48 @@
-{ lib, pkgs, ... }:
+{ lib, config, pkgs, ... }:
 
 with lib;
 
+let
+  cfg = config.programs.firefox;
+in
 {
   programs.firefox.enable = true;
-  programs.firefox.autoConfig = ''
-    lockPref("extensions.pocket.enabled", false)
-    lockPref("signon.rememberSignons", false)
-    lockPref("pref.general.disable_button.default_browser", true)
-    lockPref("pref.privacy.disable_button.view_passwords", true)
-  '';
+  programs.firefox = {
+    policies = {
+      DisablePocket = true;
+      DisableProfileRefresh = true;
+      DontCheckDefaultBrowser = true;
+      OfferToSaveLogins = false;
+      PasswordManagerEnabled = false;
 
-  programs.sway.extraSessionCommands = ''
-    export MOZ_ENABLE_WAYLAND=1
-    # open links using the wayland instance when possible
-    export MOZ_DBUS_REMOTE=1
-  '';
+      SearchEngines = {
+        Default = "DuckDuckGo";
+        Remove = [
+          "Google"
+          "Amazon.ca"
+          "Bing"
+          "eBay"
+          "Wikipedia (en)"
+        ];
+      };
+    };
+  };
 
-  programs.sway.include."50-firefox.conf" = ''
-    no_focus [title="Firefox — Sharing Indicator"]
-    for_window [title="Firefox — Sharing Indicator"] {
-      border none
-      floating enable
-      move position 50% 20
-      sticky enable
-    }
-  '';
+  programs.sway = mkIf cfg.enable {
+    extraSessionCommands = ''
+      export MOZ_ENABLE_WAYLAND=1
+      # open links using the wayland instance when possible
+      export MOZ_DBUS_REMOTE=1
+    '';
+
+    include."50-firefox.conf" = ''
+      no_focus [title="Firefox — Sharing Indicator"]
+      for_window [title="Firefox — Sharing Indicator"] {
+        border none
+        floating enable
+        move position 50% 20
+        sticky enable
+      }
+    '';
+  };
 }
