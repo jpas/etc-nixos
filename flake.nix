787ef18ae2ef411efa0819aaeb06f841f8d32043
Jarrod Pas <jarrod@jarrodpas.com>
2024-02-23 12:48:47 -0500

    temporarily go to nixpkgs/master for imv-4.5.0

--- flake.nix
+++ flake.nix
@@ -1,6 +1,6 @@
 {
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    nixpkgs.url = "github:nixos/nixpkgs/master";
     utils.url = "github:numtide/flake-utils";
 
     agenix = {
